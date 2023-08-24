90ed18ace7b754f602bf9388bb6f0a2ed953a28a
aleksei-commmune <ivanov.aleksei@commmune.jp>
2023-08-24 12:32:11 +0900

    add example

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,15 @@ import TabsCanvas from "./TabsCanvas";
 import { Notifications } from "./components/Notifications";
 import { ParsingError } from "./notifier/notifier";
 
+// prettier-ignore
+const example =
+`--------------------------------
+-3------------3-2h3----3-2h3----
+--2--------------------------2--
+--0------0----------0----------0
+--0-----------------------------
+--0--------0----------0---------`;
+
 function App() {
   const [tabsInput, setTabsInput] = createSignal("");
   const [parsedTabs, setParsedTabs] = createSignal<Note[][]>([]);
@@ -15,22 +24,27 @@ function App() {
     setParsingErrors(notation.map((v) => v.errors));
   };
 
+  const handleExample = () => {
+    setTabsInput(example);
+    handleParse(example);
+  };
+
   return (
     <div class="min-h-screen bg-gray-100 flex items-center justify-center">
       <div class="bg-white p-4 shadow-md rounded-md w-120">
         <h1 class="text-2xl font-semibold mb-4">Guitar Tabs Parser</h1>
         <textarea
-          class="w-full p-2 border rounded-md resize-none"
+          class="w-full p-2 border rounded-md resize-none font-mono"
           rows="10"
           value={tabsInput()}
           onInput={(e) => handleParse(e.target.value)}
         ></textarea>
         <button
-          class="mt-2 bg-blue-500 text-white py-2 px-4 rounded-md hidden
+          class="mt-2 bg-blue-500 text-white py-2 px-4 rounded-md
           hover:bg-blue-600 active:bg-blue-700 transition-colors duration-100 cursor-pointer"
-          // onClick={handleParse}
+          onClick={handleExample}
         >
-          Parse Tabs
+          Paste example
         </button>
         <div class="mt-4">
           <TabsCanvas
